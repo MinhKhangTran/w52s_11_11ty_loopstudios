3d888f6d43f9b7cd0e8bf8c6150390dcd24e40e7
MinhKhangTran <minhkhang.tran@gmx.de>
2022-03-16 05:53:45 +0100

    :sparkles: footer and fix other stuffs

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/js");
+
+  eleventyConfig.addShortcode("ext", function (name, link) {
+    return `<a class="footer__social" href="${link}" title="${link}" target="_blank" rel="noopener">
+              <img src="/images/icon-${name}.svg" alt="${name}"/>
+            </a>`;
+  });
   return {
     dir: {
       input: "src",
